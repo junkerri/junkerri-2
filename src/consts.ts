ea243fb0f98018b52873cc6773de82d6a803b112
junkerri <junkerri@users.noreply.github.com>
2026-02-03 13:50:37 -0600

    headline change

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Aastha Kumari Karki";
+export const SITE_TITLE = "Junkerri | Official Website";
 export const SITE_DESCRIPTION = "Developer, Designer & Digital Creator";
